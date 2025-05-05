81ebc53b6a74e326d1b447e4aed2ffc35991d063
Mauro Falcone <maurofalcone.95@gmail.com>
2025-05-05 20:39:50 -0300

    Updated nextjs config to fetch external images & parse svg as components

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
+  images: {
+    domains: ["dummyjson.com"],
+  },
 };
 
 export default nextConfig;
